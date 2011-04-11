2cb98259f056047caa2fd8b277997d317fb12192
Max Maischein <corion@corion.net>
2011-04-11 21:39:10 +0200

    Start moving towards a CPAN release

--- bin/ffeedflotr.pl
+++ bin/ffeedflotr.pl
@@ -4,6 +4,9 @@ use WWW::Mechanize::Firefox;
 use Getopt::Long;
 use Time::Local;
 use Data::Dumper;
+use vars qw($VERSION);
+
+$VERSION = '0.01';
 
 =head1 NAME
 
---
+++ lib/App/Ffeedflotr.pm
@@ -0,0 +1,18 @@
+package App::ffeedflotr;
+use vars qw($VERSION);
+
+$VERSION = '0.01';
+
+=head1 NAME
+
+App::ffeedflotr - plot data in Firefox using flot
+
+=head1 SYNOPSIS
+
+This module currently is just a placeholder
+to accompany the C<ffeedflotr.pl> program in this
+distribution.
+
+=cut
+
+1;
