af3487f7a7a2dbf6bc0bcca043a04632a0c8448d
Max Maischein <corion@corion.net>
2011-04-12 18:59:56 +0200

    (Re)generate images

--- images/generate-images.cmd
+++ images/generate-images.cmd
@@ -7,3 +7,8 @@ perl -w bin\ffeedflotr.pl t\cpantesters.txt --time -o images\simple-plot-time-CP
 
 perl -w bin\ffeedflotr.pl t\cpantesters.txt --time --fill -o images\simple-plot-filled-CPAN-Testers.png
 perl -w bin\ffeedflotr.pl t\cpantesters.txt --time --fill --xlabel "CPAN Test Results" -o images\simple-plot-label-CPAN-Testers.png
+
+perl -w bin\ffeedflotr.pl t\stats1.txt --time --sep=, --fill -o images\multi-plot-CPAN-Testers-time-long.png
+perl -w bin\ffeedflotr.pl t\stats1.txt --time --sep=, --xlen 48 --fill -o images\multi-plot-CPAN-Testers-time-short.png
+
+perl -w bin\ffeedflotr.pl t\stats1.txt --time --sep=, --xlen 48 --fill --legend 1=Uploads --legend 2=Reports --legend 3=Pass --legend 4=Fail -o images\multi-plot-CPAN-Testers-time-legend.png
